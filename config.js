3049f5229c6f4dd65fbff9943e7c35b6c7d3afa7
arsha8899 <79307436+arsha8899@users.noreply.github.com>
2021-08-18 21:10:59 +0430

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "YOUR_DISCORD_BOT_TOKEN",
+    channel: "XXXXXXXXXXXXXXXXXX",
+    messageTemplate: "Video Jadid To Channel Upload Shod\n{url}",
+    channel_id: "YOUTUBE_CHANNEL_ID",
+    watchInterval: 5000
+}
