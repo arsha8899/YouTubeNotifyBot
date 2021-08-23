cf1faf6d4fd96c10abf06402c791753d4244a9b4
arsha8899 <79307436+arsha8899@users.noreply.github.com>
2021-08-23 16:09:13 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "ODc3NTk0NzExMjUzMjE3MzEw.YR05xA.ttHifP5fN8bBHHGTgjwoJtAMKJs",
-    channel: "news",
+    token: "token",
+    channel: "876128428959629383",
     messageTemplate: "Video Jadid To Channel Upload Shod\n{url}",
     channel_id: "UCAZzi7JAnfvprsDjM0kRT3g",
     watchInterval: 5000
